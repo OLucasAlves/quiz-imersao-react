f9a8466cbfe6fc28510acee02f736d3668df9f56
Lucas Ribeiro <lucas.alves6540@gmail.com>
2021-01-25 10:53:57 -0300

    Iniciando o projeto

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,52 @@
 import styled from 'styled-components'
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import QuizBackground from '../src/components/QuizBackground';
+import QuizLogo from '../src/components/QuizLogo';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <Widget>
+          <Widget.Content>
+            <Widget.Header>
+              <h1>Aqui um titulo legal</h1>
+            </Widget.Header>
+
+            <p>Aqui uma descrição legal</p>
+          </Widget.Content>
+
+        </Widget>
+
+        <Widget>
+          <Widget.Content>
+            <Widget.Header>
+              <h1>Aqui um titulo legal</h1>
+            </Widget.Header>
+
+
+            <p>Aqui uma descrição legal</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+        
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/OLucasAlves" /> 
+    </QuizBackground>
+  );
 }
